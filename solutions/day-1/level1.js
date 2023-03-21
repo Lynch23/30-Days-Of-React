b64245a3f05a9f6e71ed026757848965354a2001
Lynch23 <austinlynch877@gmail.com>
2023-03-21 09:08:13 +0100

    Documentation folder added

---
+++ solutions/day-1/level1.js
@@ -0,0 +1,7 @@
+const arr = [];
+const arr1 = [1, 3, 45, [34, 5, 7], 45];
+const arrLength = arr1.length;
+console.log(arr1.length);
+console.log('first element : ', arr1[0]);
+console.log('Middle element : ', arr1[arrLength/2]);
+
